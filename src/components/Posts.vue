766e8ca63169a7515f89e813b4b9af9242e8b890
eevan7a9 <e7a9@protonmail.com>
2019-08-31 02:51:15 +0900

    use vuex for post with axios

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,12 +1,27 @@
 <template>
   <main>
-    <h1>Posts</h1>
+    <div class="post-cont">
+      <h1>{{allPosts}}</h1>
+    </div>
   </main>
 </template>
 
 <script>
+import { mapGetters, mapActions } from "vuex";
 export default {
-  name: "Posts"
+  name: "Posts",
+  data() {
+    return {
+      name: "evan"
+    };
+  },
+  methods: {
+    ...mapActions(["getPosts"])
+  },
+  computed: mapGetters(["allPosts"]),
+  created() {
+    this.getPosts();
+  }
 };
 </script>
 
--- src/store.js
+++ src/store.js
@@ -1,16 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import posts from './modules/posts'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
+  modules: {
+    posts,
   }
 })
