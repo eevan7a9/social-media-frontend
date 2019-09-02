b506b1e8d1cb998d6aed05163172d3366f5cc9af
eevan7a9 <e7a9@protonmail.com>
2019-09-03 03:04:12 +0900

    added details component

---
+++ src/components/PostDetails.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>{{id}}</main>
+</template>
+
+<script>
+export default {
+  name: "PostDetails",
+  props: {
+    id: Number
+  },
+  created() {
+    console.log(this.id);
+  }
+};
+</script>
+
+<style>
+</style>
