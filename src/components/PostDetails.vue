861470cf6390f8107f04094077ef3cdf5245e79b
eevan7a9 <e7a9@protonmail.com>
2019-09-04 06:28:50 +0900

    added icons and return redirection

--- src/components/PostDetails.vue
+++ src/components/PostDetails.vue
@@ -1,13 +1,26 @@
 <template>
   <main class="container">
     <div class="post-container">
+      <div class="back" @click="returnHome">
+        <img src="../assets/icons/x.svg" alt srcset />
+      </div>
       <div class="created-date">
         <small>{{postDetails.created_at}}</small>
       </div>
       <hr />
-      <div class="title">
+      <div class="title" v-if="!update">
         <h4>{{postDetails.user_username}} -</h4>
         <p>{{postDetails.title}}</p>
+      </div>
+      <div class="to-update" v-if="update">
+        <h4>{{postDetails.user_username}} -</h4>
+        <textarea
+          v-model="postDetails.title"
+          id="title"
+          cols="30"
+          rows="5"
+          @keypress.enter="submit"
+        ></textarea>
       </div>
       <div class="post-bottom">
         <div class="star-comment">
@@ -17,10 +30,20 @@
           <span>{{postDetails.comments}}</span>
         </div>
         <div class="post-options">
-          <ul>
-            <li>edit</li>
-            <li @click="delPost">delete</li>
-            <li>report</li>
+          <ul v-if="!update">
+            <li @click="edit" class="color-green">
+              <img src="../assets/icons/edit.svg" alt="edit" />
+            </li>
+            <li @click="delPost" class="color-red">
+              <img src="../assets/icons/trash-2.svg" alt="delete" />
+            </li>
+            <li class="color-muted">
+              <img src="../assets/icons/flag.svg" alt="report" srcset />
+            </li>
+          </ul>
+          <ul class="update-option" v-if="update">
+            <li class="cancel" @click="cancel">Cancel</li>
+            <li class="submit" @click="submit">Submit</li>
           </ul>
         </div>
       </div>
@@ -36,16 +59,30 @@ export default {
     id: Number
   },
   data() {
-    return {};
+    return {
+      update: 0
+    };
   },
   computed: {
     ...mapGetters(["postDetails"])
   },
   methods: {
-    ...mapActions(["viewPost", "deletePost"]),
-
+    ...mapActions(["viewPost", "deletePost", "editPost"]),
+    cancel() {
+      this.update = 0;
+    },
     delPost() {
       this.deletePost(this.postDetails.id);
+      this.$router.push({ name: "home", query: { redirect: "/" } });
+    },
+    edit() {
+      this.update = !this.update;
+    },
+    submit() {
+      this.editPost(this.postDetails);
+      this.update = 0;
+    },
+    returnHome() {
       this.$router.push({ name: "home", query: { redirect: "/" } });
     }
   },
@@ -60,7 +97,11 @@ hr {
   color: #45ad78;
   margin: 5px 0;
 }
-
+.back {
+  padding-left: 20px;
+  padding-top: 10px;
+  cursor: pointer;
+}
 .created-date {
   width: 100%;
   text-align: right;
@@ -82,7 +123,6 @@ hr {
 .post-options li {
   padding: 5px 30px 5px 30px;
   text-align: center;
-  color: #333;
   text-transform: capitalize;
   font-style: italic;
   cursor: pointer;
