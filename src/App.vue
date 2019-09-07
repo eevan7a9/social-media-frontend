a60e85fa5656ee93a618ab1afe282eeed4a323c6
eevan7a9 <e7a9@protonmail.com>
2019-09-07 21:27:08 +0900

    added TopNavBar component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav" class="bg-vuedient">
-      <div class="container">
-        <router-link to="/" class="nav-item color-white">Home</router-link>
-        <router-link to="/about" class="nav-item color-white">About</router-link>
-      </div>
-    </div>
+    <TopNavBar />
     <transition name="fade" mode="out-in">
       <router-view class="container" />
     </transition>
   </div>
 </template>
 <script>
+import TopNavBar from "./components/TopNavBar";
 import { mapActions, mapGetters } from "vuex";
 export default {
+  components: {
+    TopNavBar
+  },
   methods: {
     ...mapActions(["getPosts", "getLikes", "getUsers", "getAllComments"])
   },
@@ -43,15 +42,5 @@ body {
 #app {
   font-family: "Roboto", sans-serif;
   color: #333;
-}
-#nav {
-  line-height: 40px;
-}
-.nav-item {
-  text-decoration: none;
-  font-size: 17px;
-  letter-spacing: 1px;
-  font-weight: 600;
-  padding: 0 10px;
 }
 </style>
---
+++ src/components/TopNavBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="nav" class="bg-vuedient">
+    <div class="container">
+      <router-link to="/" class="nav-item color-white">Home</router-link>
+      <router-link to="/about" class="nav-item color-white">About</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TopNavBar"
+};
+</script>
+
+<style scoped>
+#nav {
+  line-height: 40px;
+}
+.nav-item {
+  text-decoration: none;
+  font-size: 17px;
+  letter-spacing: 1px;
+  font-weight: 600;
+  padding: 0 10px;
+}
+</style>
