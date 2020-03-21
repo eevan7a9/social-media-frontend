08b5eff0498dfe6a2cf74fcd86e31b694bac8b03
eevan7a9 <e7a9@protonmail.com>
2020-03-22 05:33:05 +0900

    updated about

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,15 +2,20 @@
   <div class="about-container">
     <h1>SocialVue</h1>
     <div class="status-wrapper">
-      <img class="status" src="../assets/status.svg" alt="status">
+      <img class="status" src="../assets/status.svg" alt="status" />
     </div>
     <h3>What is this?</h3>
     <p class="first-parg">Is a Social Media Application.</p>
-    <p>Social media are interactive computer-mediated technologies that facilitate the creation and sharing of information, ideas, career interests and other forms of expression via virtual communities and networks.</p>
+    <p>
+      Social media are interactive computer-mediated technologies that
+      facilitate the creation and sharing of information, ideas, career
+      interests and other forms of expression via virtual communities and
+      networks.
+    </p>
     <h3>What can You do?</h3>
     <div class="what-can">
       <div class="image">
-        <img src="../assets/question.svg" alt="question">
+        <img src="../assets/question.svg" alt="question" />
       </div>
       <div class="words">
         <h4>As a Visitor -</h4>
@@ -32,14 +37,17 @@
           <li>
             You
             <i>
-              can give
-              Stars
-            </i> to Posts you like
+              can give Stars
+            </i>
+            to Posts you like
           </li>
           <li>
             You
             <i>can remove Stars</i> to Posts you unlike
           </li>
+          <li>You can view users profile</li>
+          <li>You can follow other user you like</li>
+          <li>You can filter posts</li>
         </ul>
       </div>
     </div>
@@ -68,17 +76,27 @@
           </li>
           <li>
             JSONPlaceholder -
-            <i>a free online REST API that you can use whenever you need some fake data.</i>
+            <i
+              >a free online REST API that you can use whenever you need some
+              fake data.</i
+            >
+          </li>
+          <li>
+            SweetAlert2 -
+            <i>
+              A beautiful, responsive, customizable, accessible (WAI-ARIA)
+              replacement for JavaScript's popup boxes</i
+            >
           </li>
         </ul>
       </div>
-       <img src="../assets/code.svg" alt="code">
+      <img src="../assets/code.svg" alt="code" />
     </div>
     <section class="src">
       <p>@2019 :</p>
-      <a
-        href="https://github.com/eevan7a9/social-media-frontend"
-      >https://github.com/eevan7a9/social-media-frontend</a>
+      <a href="https://github.com/eevan7a9/social-media-frontend"
+        >https://github.com/eevan7a9/social-media-frontend</a
+      >
     </section>
   </div>
 </template>
@@ -112,25 +130,25 @@ li {
   justify-content: center;
   width: 100%;
 }
-.status-wrapper{
+.status-wrapper {
   display: flex;
   justify-content: center;
 }
-.status{
+.status {
   height: 400px;
 }
-.what-can{
+.what-can {
   display: grid;
   grid-template-columns: 1fr 1fr;
 }
-.what-can .image img{
+.what-can .image img {
   width: 100%;
 }
-.technology{
+.technology {
   display: grid;
   grid-template-columns: 1fr 1fr;
 }
-.technology img{
+.technology img {
   width: 100%;
   height: 300px;
 }
