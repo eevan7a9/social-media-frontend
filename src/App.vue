f7da72d4aad8164df80dfd7356945d86eaa65ec8
eevan7a9 <eevan7a9@gmail.com>
2025-02-03 03:41:45 +0800

    chore: rebuild base tailwindcss4 Vue3 Typescript Pinia

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,20 @@
-<template>
-  <div id="app">
-    <TopNavBar />
-    <FullPageLoader v-show="showLoader" />
-    <transition name="fade" mode="out-in">
-      <router-view :key="$route.fullPath" class="container" />
-    </transition>
-  </div>
-</template>
-<script>
-import TopNavBar from "./components/TopNavBar";
-import FullPageLoader from "./components/FullPageLoader";
-import { mapGetters } from "vuex";
-export default {
-  components: {
-    TopNavBar,
-    FullPageLoader
-  },
-  computed: mapGetters(["showLoader"])
-};
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
 </script>
-<style>
-@import url("https://fonts.googleapis.com/css?family=Roboto&display=swap");
-body {
-  background-image: radial-gradient(
-    circle farthest-corner at 18.7% 37.8%,
-    rgba(250, 250, 250, 1) 0%,
-    rgba(225, 234, 238, 1) 90%
-  );
-}
-#app {
-  font-family: "Roboto", sans-serif;
-  color: #333;
-}
-</style>
+
+<template>
+  <header>
+    <nav class="flex w-full bg-white py-3 lg:py-4 max-w-screen-lg mx-auto">
+      <ul class="flex gap-3 mr-0 ml-auto">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/profile">Profile</RouterLink>
+      </ul>
+    </nav>
+  </header>
+
+  <main class="block bg-light dark:bg-dark min-h-screen">
+    <RouterView />
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/views/ProfileView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="profile">
+    <h1 class="test">This is an profile page</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.test {
+  color: red;
+}
+</style>
