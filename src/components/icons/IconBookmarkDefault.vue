ef54b5813444657d0ddeef89b992d0f13821446f
eevan7a9 <eevan7a9@gmail.com>
2025-02-06 19:01:32 +0800

    chore: added more Icons

---
+++ src/components/icons/IconBookmarkDefault.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+defineProps<{ filled?: boolean }>();
+</script>
+
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" v-if="filled">
+    <title>bookmark-check</title>
+    <path
+      d="M17,3A2,2 0 0,1 19,5V21L12,18L5,21V5C5,3.89 5.9,3 7,3H17M11,14L17.25,7.76L15.84,6.34L11,11.18L8.41,8.59L7,10L11,14Z"
+    />
+  </svg>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" v-else>
+    <title>bookmark-outline</title>
+    <path d="M17,18L12,15.82L7,18V5H17M17,3H7A2,2 0 0,0 5,5V21L12,18L19,21V5C19,3.89 18.1,3 17,3Z" />
+  </svg>
+</template>
--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -10,8 +10,38 @@ import IconThumbUp from './IconThumbUp.vue';
 import IconBookmark from './IconBookmark.vue';
 import IconVerticalDots from './IconVerticalDots.vue';
 import IconHorizontalDots from './IconHorizontalDots.vue';
+import IconImage from './IconImage.vue';
+import IconMapPin from './IconMapPin.vue';
+import IconLink from './IconLink.vue';
+import IconEmoji from './IconEmoji.vue';
+import IconShare from './IconShare.vue';
+import IconChat from './IconChat.vue';
+import IconChat2 from './IconChat2.vue';
+import IconBookmarkDefault from './IconBookmarkDefault.vue';
+import IconHelp from './IconHelp.vue';
+import IconCog from './IconCog.vue';
+import IconBell from './IconBell.vue';
+import IconPersonCircle from './IconPersonCircle.vue';
+import IconMagnify from './IconMagnify.vue';
+import IconClose from './IconClose.vue';
+import IconFlag from './IconFlag.vue';
 
 export {
+  IconFlag,
+  IconClose,
+  IconMagnify,
+  IconPersonCircle,
+  IconBell,
+  IconCog,
+  IconHelp,
+  IconBookmarkDefault,
+  IconChat,
+  IconChat2,
+  IconShare,
+  IconMapPin,
+  IconEmoji,
+  IconLink,
+  IconImage,
   IconHorizontalDots,
   IconVerticalDots,
   IconBookmark,
